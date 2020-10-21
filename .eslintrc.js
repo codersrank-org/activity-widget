ad6c093e967fb271661565fa893860e4ecb31592
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-10-21 12:27:13 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
