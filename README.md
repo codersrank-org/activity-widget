52ea68dab3825f28abbb5fa023fcecaf60969880
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-12-04 11:42:59 +0300

    Add image API

--- README.md
+++ README.md
@@ -148,6 +148,33 @@ function onData(event) {
 document.querySelector('#activity').addEventListener('data', onData);
 ```
 
+## Use As Image
+
+It is also possible to insert Activity widget as an image. It is useful in places where you can't integrate web component, or for example on your GitHub profile README.md page.
+
+Image URL is the following:
+
+```
+https://cr-ss-service.azurewebsites.net/api/ScreenShot?widget=activity&username=YOUR_USERNAME
+```
+
+It accepts all widget attributes as query string parameters, plus one extra parameter:
+
+| Name    | Type     | Default | Description                                                                                                                                                                     |
+| ------- | -------- | ------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `width` | `number` | `800`   | Width of widget element (generated image). Note that generated image has @2x pixel density, so the PNG image will be actually generated in @2x size from the one specified here |
+| `style` | `string` |         | `style` attribute value (here you can specify all CSS variables)                                                                                                                |
+
+For example:
+
+```html
+<img
+  src="https://cr-ss-service.azurewebsites.net/api/ScreenShot?widget=activity&username=YOUR_USERNAME&labels=true"
+/>
+```
+
+Note that you need to URL Encode some of the characters, for example `#` should be `%23` and `#ff0` color should be specified as `%23ff0` in query.
+
 ## Contribution
 
 Yes please! See the [contributing guidelines](https://github.com/codersrank-org/activity-widget/blob/master/CONTRIBUTING.md) for details.
---
+++ api/index.js
@@ -0,0 +1,94 @@
+const puppeteer = require('puppeteer');
+
+const escape = (string = '') => {
+  return string.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+};
+
+const getContent = (attrs = {}) => {
+  let attrsString = '';
+  Object.keys(attrs).forEach((attrName) => {
+    const attrValue = attrs[attrName];
+    if (!attrValue) return;
+    attrsString += ` ${escape(attrName)}="${escape(attrValue)}" `;
+  });
+  return /* html */ `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CodersRank Activity</title>
+    <style>
+      html, body {
+        margin: 0;
+        padding: 0;
+        font-family: Open Sans, Helvetica Neue, Helvetica, Arial, sans-serif;
+        position: relative;
+      }
+      codersrank-activity {
+        width: 800px;
+      }
+    </style>
+  </head>
+  <body>
+    <codersrank-activity ${attrsString}></codersrank-activity>
+    <script src="https://unpkg.com/@codersrank/activity/codersrank-activity.min.js"></script>
+  </body>
+  </html>
+  `;
+};
+
+const getScreenshot = async (attrs = {}) => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 800, height: 600, deviceScaleFactor: 2 });
+  await page.setContent(getContent(attrs), { waitUntil: 'networkidle0' });
+  const widgetElement = await page.$('codersrank-activity');
+  const screenshotBuffer = await widgetElement.screenshot({ omitBackground: true });
+  await browser.close();
+  return screenshotBuffer;
+};
+
+module.exports = async (context, req) => {
+  const functionStartTime = new Date().getTime();
+  if (!req.query.username) {
+    context.res = {
+      status: 200,
+      headers: {
+        'content-type': 'text/html',
+      },
+      body: '<h1>Error</h1><p><b>username</b> is required in query string</p>',
+    };
+    return;
+  }
+
+  const attrs = {
+    ...(req.query || {}),
+  };
+  if (!attrs.style) attrs.style = '';
+  if (attrs.width) {
+    attrs.style += `;width:${attrs.width}px`;
+  }
+  attrs.style = decodeURIComponent(attrs.style);
+
+  const screenshotBuffer = await getScreenshot(attrs);
+
+  context.res = {
+    headers: {
+      'content-type': 'image/png',
+      'cache-control': 'no-cache, no-store, must-revalidate',
+      expires: '0',
+    },
+    body: screenshotBuffer,
+  };
+  const functionTime = new Date().getTime() - functionStartTime;
+  // eslint-disable-next-line
+  console.log(
+    JSON.stringify({
+      ...attrs,
+      timeTook: `${functionTime / 1000}s`,
+    }),
+  );
+};
